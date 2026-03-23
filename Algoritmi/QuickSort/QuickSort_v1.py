5d1e6be66f9d117748f1c724cb52781e564254a7
g620an <gabriele.iue2@gmail.com>
2026-03-23 17:33:27 +0100

    QuickSort_v1

---
+++ Algoritmi/QuickSort/QuickSort_v1.py
@@ -0,0 +1,47 @@
+from random import Random
+
+def partiziona(v, ini:int, fin:int):
+    pivot = Random().randrange(ini, fin)
+    temp = v[pivot]
+    v[pivot] = v[ini]
+    v[ini] = temp
+    inf = ini + 1
+    sup = fin
+    while(inf < sup):
+        while(inf<=sup and v[inf]<v[ini]):
+            inf += 1
+        while(inf<=sup and v[sup]>=v[ini]):
+            sup -= 1
+        if(inf<sup):
+            temp = v[inf]
+            v[inf] = v[sup]
+            v[sup] = temp
+    temp = v[sup]
+    v[sup] = v[ini]
+    v[ini] = temp
+    return sup #Posizione di v[ini] cioè la posizione naturale del PIVOT
+
+def sort(v, ini:int, fin:int):
+    if fin-ini+1 <= 1:
+        return
+    else:
+        p = partiziona(v , ini , fin)
+        sort(v , ini , p-1)
+        sort(v , p+1 , fin)
+
+
+if __name__ == "__main__":
+    v = [9 , 4, 2, 7, 0, -1, -11, 3, 4, 2, 8]
+    #scelgo 4
+    #[4,9,2,7,0] inf = 1 sup = 4
+    #[4,9,2,7,0] inf = 1 e sup=4 siamo stati sfortunati
+    #[4,0,2,7,9] inf = 1 sup = 4
+    #[4,0,2,7,9] inf = 2 sup = 2
+    #[2,0,4,7,9] già parzialmente ordinato p = 2
+    #[2,0] [4] [7,9]
+    #[2,0] scelgo 0 inf = 1 sup = 1
+    sort(v , 0 , len(v)-1)
+    print(v)
+
+
+
