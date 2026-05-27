fc662427078ff6c330755e904c8f4ff619b420f3
Gabriele Iuele <gabriele.iue2@gmail.com>
2026-05-27 10:21:36 +0200

    ColoraGrafo

--- Algoritmi/SubsetSum/SubsetSum.py
+++ Algoritmi/SubsetSum/SubsetSum.py
@@ -1,7 +1,10 @@
-class SubsetSum:
+#Stato -> sol[i] = True se elemento[i] è contenuto nella soluzione parziale
+#livello -> elemento con cui lavoro
+#Dominio -> True o False se prendere o non prendere un elemento
+#Vincolo parziale -> Somma <= K 
+#Soluzione completa -> Somma era uguale a K
+
+class SubsetSum(Backtracking):
     def __init__(self):
         pass
-    
-    def calcola(set, k):
-        scelte = []
-        
+    #Bisogna implementare i metodi che non ci sono nella classe Backtracking
---
+++ Esercizi/colorazioneGrafo.py
@@ -0,0 +1,12 @@
+#Colorare il grafo in modo tale che in nodi adiacenti non ci siano gli stessi colori
+#Colori: Rosso, Blue e Verde
+
+#livello -> Nodo da colorare
+#scelte -> Colori (se ho C colori le scelte vanno da 0 a C-1)
+#Vincolo -> Il colore del nodo livello deve essere diverso dai suoi adiacenti
+#Soluzione -> Una lista  che va da 0->n dove per ogni valore (ID nodo) abbiamo il colore
+class Coloratore(Backtrackingq):
+    def __init__(self):
+        pass
+    def coloraGrafo(G):
+        pass
