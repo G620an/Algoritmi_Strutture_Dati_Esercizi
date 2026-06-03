bf95e6bb47de69351d1a3fb603ad711d41bc2af2
g620an <gabriele.iue2@gmail.com>
2026-06-03 09:53:10 +0200

    Esercizio verificaSomma.py ; completo e funzionante

---
+++ Algoritmi/Esercizi/verificaSomma.py
@@ -0,0 +1,25 @@
+#restituisce true <=> se la somma dei valori contenuti nei nodi foglia di a è uguale alla somma dei nodi al livello l di a
+
+from Alberi.Albero import Albero
+
+def verificaSomma(a:Albero, l:int):
+    if a is None:
+        return False
+    sl = 0; sf = 0
+    somma(a, 0, l, sl, sf)
+    return sl == sf
+
+def somma(a:Albero, h:int, l:int, sl, sf):
+    if a is None: return
+    if h == l:
+        sl += a.info
+    if a.sx is None and a.dx is None:
+        sf += a.info
+        return
+    somma(a.sx, h+1, l, sl, sf)
+    somma(a.dx, h+1, l, sl, sf)
+
+#Analisi di complessità
+#CTP(n) == CTM(n) = Qualsiasi tipo di albero, dobbiamo scorrere tutti i nodi (nessuno sconto) tetha(n)
+#CSP(n) = Albero degenere sx e quindi n record di attivazione tutti attivi sulla somma(a.sx...) tetha(n)
+#CSM(n) = Albero bilanciato e quindi al più log(n) record di attivazione attivi contemporaneamente tetha(log(n))
